cdda729a653b8bc363464ecdc93c8b57b5c8f1a0
cyrilsl <cyrilsl@github.com>
2024-02-18 01:34:18 +0530

    added nav and footer, and subdomain.apexDomain to display

--- src/app/[subdomain]/page.tsx
+++ src/app/[subdomain]/page.tsx
@@ -2,7 +2,6 @@
 import Image from "next/image";
 import React from "react"
 import { useProducts } from "medusa-react"
-import { useAdminStore } from "medusa-react"
 
 export default function Storefront({
   params,
@@ -10,12 +9,14 @@ export default function Storefront({
   params: { subdomain: string };
 })  {
     const { products, isLoading } = useProducts()
+  console.log("Products : ",products)
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL.replace(/^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)/, '').replace(/\/$/, '');
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-        Domain URL : {params.subdomain}
+        Domain URL : {params.subdomain}.{baseUrl}
           
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">

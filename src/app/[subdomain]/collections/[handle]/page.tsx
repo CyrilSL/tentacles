aef1966a9e18b3830628755ed4b90a2387422f94
cyrilsl <cyrilsl@github.com>
2024-02-20 11:03:45 +0530

    fixed collection bug

--- src/app/[subdomain]/collections/[handle]/page.tsx
+++ src/app/[subdomain]/collections/[handle]/page.tsx
@@ -1,82 +1,31 @@
+import { getCollectionByHandle } from "@lib/data"
+import CollectionTemplate from "@modules/collections/templates"
 import { Metadata } from "next"
 import { notFound } from "next/navigation"
 
-import {
-  getCollectionByHandle,
-  getCollectionsList,
-  listRegions,
-} from "@lib/data"
-import CollectionTemplate from "@modules/collections/templates"
-import { SortOptions } from "@modules/store/components/refinement-list/sort-products"
-
 type Props = {
-  params: { handle: string; countryCode: string }
-  searchParams: {
-    page?: string
-    sortBy?: SortOptions
-  }
-}
-
-export const PRODUCT_LIMIT = 12
-
-export async function generateStaticParams() {
-  const { collections } = await getCollectionsList()
-
-  if (!collections) {
-    return []
-  }
-
-   // @ts-ignore: Unreachable code error
-  const countryCodes = await listRegions().then((regions) =>
-    regions?.map((r) => r.countries.map((c) => c.iso_2)).flat()
-  )
-
-  const collectionHandles = collections.map((collection) => collection.handle)
-
-  const staticParams = countryCodes
-    ?.map((countryCode) =>
-      collectionHandles.map((handle) => ({
-        countryCode,
-        handle,
-      }))
-    )
-    .flat()
-
-  return staticParams
+  params: { handle: string }
 }
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  const collection = await getCollectionByHandle(params.handle)
+  const { collections } = await getCollectionByHandle(params.handle)
+
+  const collection = collections[0]
 
   if (!collection) {
     notFound()
   }
 
-  const metadata = {
+  return {
     title: `${collection.title} | Medusa Store`,
     description: `${collection.title} collection`,
-  } as Metadata
-
-  return metadata
+  }
 }
 
-export default async function CollectionPage({ params, searchParams }: Props) {
-  const { sortBy, page } = searchParams
+export default async function CollectionPage({ params }: Props) {
+  const { collections } = await getCollectionByHandle(params.handle)
 
-  const collection = await getCollectionByHandle(params.handle).then(
-    (collection) => collection
-  )
+  const collection = collections[0]
 
-  if (!collection) {
-    notFound()
-  }
-
-  return (
-    <CollectionTemplate
-      collection={collection}
-      page={page}
-      sortBy={sortBy}
-      countryCode={params.countryCode}
-    />
-  )
+  return <CollectionTemplate collection={collection} />
 }

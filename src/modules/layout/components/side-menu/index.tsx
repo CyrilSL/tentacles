28b82464d2b68de8104b0ef49576fdac7bd41641
cyrilsl <lucascyrilsamuel@gmail.com>
2024-03-04 01:03:57 +0530

    Sidebar has <Link>

--- src/modules/layout/components/side-menu/index.tsx
+++ src/modules/layout/components/side-menu/index.tsx
@@ -5,7 +5,7 @@ import { ArrowRightMini, XMark } from "@medusajs/icons"
 import { Region } from "@medusajs/medusa"
 import { Text, clx, useToggleState } from "@medusajs/ui"
 import { Fragment } from "react"
-
+import Link from 'next/link'
 import LocalizedClientLink from "@modules/common/components/localized-client-link"
 import CountrySelect from "../country-select"
 
@@ -53,13 +53,13 @@ const SideMenu = ({ regions }: { regions: Region[] | null }) => {
                       {Object.entries(SideMenuItems).map(([name, href]) => {
                         return (
                           <li key={name}>
-                            <LocalizedClientLink
+                            <Link
                               href={href}
                               className="text-3xl leading-10 hover:text-ui-fg-disabled"
                               onClick={close}
                             >
                               {name}
-                            </LocalizedClientLink>
+                            </Link>
                           </li>
                         )
                       })}

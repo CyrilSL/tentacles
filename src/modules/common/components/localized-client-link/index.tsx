23cac04a0fd7742e4171063c5d6172cdc54b89e0
cyrilsl <lucascyrilsamuel@gmail.com>
2024-03-04 01:14:53 +0530

    Localized behaives like a regular <Link>

--- src/modules/common/components/localized-client-link/index.tsx
+++ src/modules/common/components/localized-client-link/index.tsx
@@ -1,32 +1,26 @@
-"use client"
-
-import Link from "next/link"
-import { useParams } from "next/navigation"
-import React from "react"
+import Link from "next/link";
+import React from "react";
 
 /**
- * Use this component to create a Next.js `<Link />` that persists the current country code in the url,
- * without having to explicitly pass it as a prop.
+ * Use this component as a simplified wrapper around the Next.js `<Link />` component.
  */
 const LocalizedClientLink = ({
   children,
   href,
   ...props
 }: {
-  children?: React.ReactNode
-  href: string
-  className?: string
-  onClick?: () => void
-  passHref?: true
-  [x: string]: any
+  children?: React.ReactNode;
+  href: string;
+  className?: string;
+  onClick?: () => void;
+  passHref?: true;
+  [x: string]: any;
 }) => {
-  const { countryCode } = useParams()
-
   return (
-    <Link href={`/${countryCode}${href}`} {...props}>
+    <Link href={href} {...props}>
       {children}
     </Link>
-  )
-}
+  );
+};
 
-export default LocalizedClientLink
+export default LocalizedClientLink;

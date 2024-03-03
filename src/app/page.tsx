eb09b0f1eb796a9c64baab750464a4677f698b25
cyrilsl <lucascyrilsamuel@gmail.com>
2024-03-03 17:51:06 +0530

    front page change to show v1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -121,13 +121,13 @@ export default function Storefront({
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
+            Version 1{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+            Version One, With upadted upstream 
           </p>
         </a>
 
